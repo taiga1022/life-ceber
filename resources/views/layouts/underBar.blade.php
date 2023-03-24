309cb214c5c4387e48f0a480e121d1ff62778ef1
reiokirenai <hellofromhere24h@gmail.com>
2023-03-24 14:15:17 +0800

    create original parts
    基本部分のbladeファイル作成

---
+++ resources/views/layouts/underBar.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./app.css">
+</head>
+<body>
+    <div class="under-tag">
+        <a href="#"><img src="./philippine-peso (4).png" alt=""></a>
+        <a href="#"><img src="./book.png" alt=""></a>
+        <a href="#"><img class="writing" src="./writing.png" alt=""></a>
+        <a href="#"><img src="./user (1).png" alt=""></a>
+    </div>
+    
+</body>
+</html>
