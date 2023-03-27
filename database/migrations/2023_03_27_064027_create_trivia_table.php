260e29db5293dce86974043fbcaa95bf265da7d5
Masato <saint.k.2525@gmail.com>
2023-03-27 14:54:12 +0800

    created Models and edited Migration Files

--- database/migrations/2023_03_27_064027_create_trivia_table.php
+++ database/migrations/2023_03_27_064027_create_trivia_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateTriviaTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('trivias', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            // $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            // $table->rememberToken();
-            $table->string('nickname')->nullable();
+            $table->string('title');
+            $table->text('content');
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('trivias');
     }
 }
