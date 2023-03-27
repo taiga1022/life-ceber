3810559dee9ce4f49ab5c4c1896b96122a59a8b4
AkaneFutai <aooooo05314159@gmail.com>
2023-03-27 13:40:44 +0800

    show
    挿入

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">  
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="show-container">
+        <img class="show-img" src="./photo/IMG-4835.JPG" alt="">
+        <h1>titletitletitle</h1>
+        <h2>address</h2>
+        <hr noshade class="show-line" >
+        <div class="show-text">     
+            テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテスト
+        </div>
+        <div class="s1">
+            <ul class="s2">
+            <li>カテゴリー：restrant</li>
+            <li>値段：$$$</li>
+            </ul>
+        </div>
+        <div class="under">
+            <div class="show-edit">編集</div>
+            <div class="show-delete">削除</div>
+            <div class="show-heart">♡</div>
+        </div>
+    </div>
+
+
+    
+</body>
+</html>
