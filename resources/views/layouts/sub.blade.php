4dd0a9783760f7345fb955a32430508788c28fca
reiokirenai <hellofromhere24h@gmail.com>
2023-03-29 15:24:34 +0800

    css
    css調整

---
+++ resources/views/layouts/sub.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/main.js') }}" defer></script>
+    
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+</head>
+<body>
+
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+</body>
+</html>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,13 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">  
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="./style.css">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.sub')
+
+{{-- ヘッダーとフッターいらない場合のbladeファイル --}}
+
+{{-- @extends('layouts.underBar') --}}
+
     <div class="show-container">
         <img class="show-img" src="./photo/IMG-4835.JPG" alt="">
         <h1>{{ $post -> place }}</h1>
@@ -22,18 +18,18 @@
                 <li>値段：{{ $post -> price }}</li>
             </ul>
         </div>
+        @if($post->user_id == Auth::user()->id)
             <div class="under">
-                <a href="{{ route('posts.edit', $post->id) }}" class="#">編集</a>
+                <a href="{{ route('posts.edit', $post->id) }}" class="show-edit">編集</a>
                 <form action="{{ route('posts.destroy', $post->id) }}" method='post'>
                     @csrf
                     @method('delete')
                     <input type="submit" value='削除' class="show-delete" onclick='return confirm("本当に削除しますか？");'>
                 </form>
+        @endif
                 <div class="show-heart">♡</div>
             </div>
     </div>
 
 
     
-</body>
-</html>
