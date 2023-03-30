91480593929eed77aee255905faf07bbf59d0e6d
reiokirenai <hellofromhere24h@gmail.com>
2023-03-30 13:54:31 +0800

    edit
    edit

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>編集</title>
-    <link rel="stylesheet" href="public\css\reset.css">
-</head>
-
-<body>
+@extends('layouts.app')
     <main>
-        <p class="create-back">←戻る</p>
+        <a href="{{ route('posts.index') }}"><p class="create-back">←戻る</p></a>
         <div class="create-container">
             <h3>編集/Edit</h3>
             <form action="{{ route('posts.update', $post->id) }}" method="POST">
@@ -20,28 +10,37 @@
                     <input type="text" name='place' class="create-input" value="{{ $post->place }}" placeholder="店名・商品名・施設名など">
                     <div class="create-title create-way">道のり</div>
                     <input type="text" name='address' class="create-input" value="{{ $post->address }}" placeholder="住所もしくは目印">
-                    <div class="create-title create-categories-header">カテゴリー</div>
-                        <ul class="create-categories">
-                            <li>restaurant</li>
-                            <li class="create-sightseeing">sightseeing</li>
-                            <li>others</li>
-                        </ul>
-                    <div class="create-title create-prices-header">価格</div>
-                        <ul class="create-prices">
-                            <li>P</li>
-                            <li class="create-pp">PP</li>
-                            <li>PPP</li>
-                        </ul>
-                    <div class="create-title create-content">内容</div>
-                    <textarea name="comment" placeholder="味の感想や雰囲気など" >{{ $post->comment }}</textarea>
-                    <p class="create-image">写真を選択</p>
-                <input type="submit" class="create-submit" value="更新">
+                    <div class="kind-box">
+                        <input type="radio" name="genre" id="res" class="radio1" value="restaurant">
+                        <label for="res">restaurant</label>
+    
+                        <input type="radio" name="genre" id="sight" class="radio1" value="sightseeing">
+                        <label for="sight">sightseeing</label>
+    
+                        <input type="radio" name="genre" id="others" class="radio1" value="others">
+                        <label for="others">others</label>
+                    </div>
+                       <div class="create-title create-prices-header">価格</div>
+                       <div class="pp-box">
+                       <input type="radio" name="price" id="box-p" class="radio" value="P">
+                       <label for="box-p">P</label>
+
+                       <input type="radio" name="price" id="box-pp" class="radio" value="PP">
+                       <label for="box-pp">PP</label>
+
+                       <input type="radio" name="price" id="box-ppp" class="radio" value="PPP">
+                       <label for="box-ppp">PPP</label>
+                    </div>
+                       <div class="create-title create-content">内容</div>
+                       <textarea name="comment" placeholder="味の感想や雰囲気など" >{{ $post->comment }}</textarea>
+                       <p class="create-image">写真を選択</p>
+                   <input type="submit" class="create-submit" value="更新">
             </form>
         </div>
     </main>
-</body>
 
-<style>
+
+{{-- <style>
     .create-container {
         text-align: center;
     }
@@ -186,5 +185,4 @@
         width: 104px;
         height: 37px;
     }
-</style>
-</html>
+</style> --}}
