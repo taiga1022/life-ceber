b68be38ecc864391ac2941f35b4518e8f2bd4a0f
AkaneFutai <aooooo05314159@gmail.com>
2023-04-04 15:27:11 +0800

    logo入れ完了

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,6 +29,7 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/posts') }}">
                     {{ config('Life-Ceber', 'Life-Ceber') }}
+                    <img class="mainlogo" src="../img/necessary/mainlogo.png" alt="" width="50px">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
