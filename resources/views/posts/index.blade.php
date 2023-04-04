a4907f731cd940038e45bcf77b8c004a7893ede3
AkaneFutai <aooooo05314159@gmail.com>
2023-04-04 14:50:25 +0800

    css微調整

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -35,6 +35,7 @@
     </div>
     @section('content')
     <div class="stickers">
+      <img class="hw" src="../img/necessary/stamps/hw.jpg" alt="宿題">
       <img class="map" src="../img/necessary/stamps/map.jpg" alt="マップ">
       <img class="bal" src="../img/necessary/stamps/bal.jpg" alt="バロット">
       <img class="boh" src="../img/necessary/stamps/boh.jpg" alt="ボホール">
@@ -45,7 +46,6 @@
       <img class="pc" src="../img/necessary/stamps/pc.jpg" alt="パンシット">
       <img class="todo" src="../img/necessary/stamps/todo.jpg" alt="やること">
       <img class="sm" src="../img/necessary/stamps/sm.jpg" alt="ビール">
-      <img class="hw" src="../img/necessary/stamps/hw.jpg" alt="宿題">
       {{-- <img class="main-logo" src="../img/necessary/stamps/hw.jpg" alt="宿題"> --}}
       <img class="main-logo" src="../img/necessary/stamps/logo.png" alt="logo">
       {{-- ↑にメインロゴを入れる！ --}}
